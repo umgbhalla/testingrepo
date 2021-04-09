dd6bbf2e84ce32f920b5f12e64b62800a3eb0cb8
umgbhalla <umg.bhalla88@gmail.com>
2021-04-09 19:26:29 +0530

    .

---
+++ 02/rp1/src/components/App.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+
+export class App extends React.Component<{},{}>{
+  render(){
+    return(
+      <div>app from class works</div>
+    )
+  }
+}
+
+
+// function App() {
+//   return (
+//     <div> app works</div>
+//   );
+// }
+
+// export default App;
